4399dc754e39899825dc5834ca008591ec362e34
jotagabrielp <53877116+jotagabrielp@users.noreply.github.com.>
2024-11-27 12:19:23 -0300

    fix: refactoring app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as redisStore from 'cache-manager-redis-store';
 import { CacheModule } from '@nestjs/cache-manager';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { EnergyMonitoringModule } from './energy-monitoring/energy-monitoring.module';
-import { WasteManagementModule } from './waste-management/waste-management.module';
-import { ReportsModule } from './reports/reports.module';
+import { ClientsModule } from '@nestjs/microservices';
 import { AuthModule } from './auth/auth.module';
 import { Energy } from './energy-monitoring/energy.entity';
 import { Waste } from './waste-management/waste.entity';
-import { SectorsModule } from './sectors/sectors.module';
 
 @Module({
   imports: [
@@ -18,36 +14,7 @@ import { SectorsModule } from './sectors/sectors.module';
       isGlobal: true,
     }),
     ClientsModule.registerAsync([
-      {
-        name: 'ENERGY_SERVICE',
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.RMQ,
-          options: {
-            urls: [configService.get<string>('RABBITMQ_URL')],
-            queue: configService.get<string>('RABBITMQ_ENERGY_QUEUE'),
-            queueOptions: {
-              durable: false,
-            },
-          },
-        }),
-      },
-      {
-        name: 'WASTE_SERVICE',
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.RMQ,
-          options: {
-            urls: [configService.get<string>('RABBITMQ_URL')],
-            queue: configService.get<string>('RABBITMQ_WASTE_QUEUE'),
-            queueOptions: {
-              durable: false,
-            },
-          },
-        }),
-      },
+
     ]),
     CacheModule.registerAsync({
       imports: [ConfigModule],
@@ -73,11 +40,7 @@ import { SectorsModule } from './sectors/sectors.module';
       }),
     }),
 
-    EnergyMonitoringModule,
-    WasteManagementModule,
-    ReportsModule,
     AuthModule,
-    SectorsModule,
   ],
 })
 export class AppModule {}
